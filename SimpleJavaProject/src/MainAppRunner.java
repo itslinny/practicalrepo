292214f92a561ee249be6b3ba4d61b36222f3f3b
Linny Poovakkattu <lpoovakk@in.ibm.com>
2020-04-14 17:02:05 +0530

    commit the player file and changes to mainapprunner

--- SimpleJavaProject/src/MainAppRunner.java
+++ SimpleJavaProject/src/MainAppRunner.java
@@ -4,7 +4,12 @@ public class MainAppRunner {
 	public static void main(String[] args) {
 		MainAppRunner m =new MainAppRunner();
 		System.out.println("helloWorld");
-
+		int sum=0;
+		
+		for(int i=0;i<10;i++) {
+			sum=sum+i;
+		}
+		System.out.println("Sum is :"+sum);
 	}
 
 }
---
+++ SimpleJavaProject/src/Player.java
@@ -0,0 +1,26 @@
+
+public class Player {
+	String name;
+	String game;
+	int age;
+	public String getName() {
+		return name;
+	}
+	public void setName(String name) {
+		this.name = name;
+	}
+	public String getGame() {
+		return game;
+	}
+	public void setGame(String game) {
+		this.game = game;
+	}
+	public int getAge() {
+		return age;
+	}
+	public void setAge(int age) {
+		this.age = age;
+	}
+	
+
+}
