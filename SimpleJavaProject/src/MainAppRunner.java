d4789701ad7b1a03663ef581c6aca2ae106a6c8c
Linny Poovakkattu <lpoovakk@in.ibm.com>
2020-04-14 17:11:16 +0530

    did changes in the additionalbranch

--- SimpleJavaProject/src/MainAppRunner.java
+++ SimpleJavaProject/src/MainAppRunner.java
@@ -5,7 +5,10 @@ public class MainAppRunner {
 		MainAppRunner m =new MainAppRunner();
 		System.out.println("helloWorld");
 		int sum=0;
-		
+		//creating a player 
+		Player p =new Player();
+		p.setName("Yash");
+		System.out.println("Player is :"+p.getName());
 		for(int i=0;i<10;i++) {
 			sum=sum+i;
 		}
